efd87e18d11acc28e6d6cda5579fd75e817f555d
Leo Gavin <leogavin123@outlook.com>
2023-12-19 10:00:21 +0530

    web-socket chat

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -17,28 +17,34 @@ export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
-  private adminSocket: Socket | null = null;
+  private adminSocketId: string | null = null;
   private clients: Map<string, Socket> = new Map();
 
-  // @SubscribeMessage('message')
-  // handleMessage(client: any, payload: any): void {
-  //   console.log(payload);
-  //   this.server.emit('message', payload); // Broadcast message to all connected clients
-  // }
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): void {
+    console.log(payload);
+    this.server.emit('message', payload); // Broadcast message to all connected clients
+  }
 
   @SubscribeMessage('connectAdmin')
   handleAdminConnection(@ConnectedSocket() client: Socket): string {
     console.log('Admin connected');
-    this.adminSocket = client;
+    this.adminSocketId = client.id; // Store the admin's socket ID
+    this.clients.set(client.id, client); // Also store the admin client in the clients map
     return 'Admin connected';
   }
 
   @SubscribeMessage('connectClient')
   handleClientConnection(
-    @MessageBody() clientId: string,
+    @MessageBody() body: { clientId: string },
+    @MessageBody() data: string,
     @ConnectedSocket() client: Socket,
   ): void {
-    this.clients.set(clientId, client);
+    console.log(data);
+    // Set the client in the map with the given clientId
+    this.clients.set(body.clientId, client);
+    console.log(`Client connected with ID: ${body.clientId}`);
+    client.emit('connectionAcknowledged', body.clientId);
   }
 
   @SubscribeMessage('sendMessageToAdmin')
@@ -46,9 +52,15 @@ export class ChatGateway {
     @MessageBody() data: { clientId: string; message: string },
   ): void {
     console.log('Received message for admin:', data);
-    if (this.adminSocket) {
-      console.log('Forwarding message to admin:', data);
-      this.adminSocket.emit('messageFromClient', data);
+
+    if (this.adminSocketId) {
+      const adminSocket = this.clients.get(this.adminSocketId);
+      if (adminSocket) {
+        console.log('Forwarding message to admin:', data);
+        adminSocket.emit('messageFromClient', data);
+      } else {
+        console.log('Admin socket not found');
+      }
     } else {
       console.log('Admin is not connected');
     }
@@ -61,20 +73,21 @@ export class ChatGateway {
     const client = this.clients.get(data.clientId);
     if (client) {
       client.emit('messageFromAdmin', data.message);
+      console.log(`Message sent to client ${data.clientId}:`, data.message);
+    } else {
+      console.log(`Client with ID ${data.clientId} not found`);
     }
   }
 
-  handleDisconnect(client: Socket): void {
-    if (this.adminSocket === client) {
-      this.adminSocket = null;
+  handleDisconnect(@ConnectedSocket() client: Socket): void {
+    if (this.adminSocketId === client.id) {
+      this.adminSocketId = null;
       console.log('Admin disconnected');
     } else {
-      this.clients.forEach((value, key) => {
-        if (value === client) {
-          this.clients.delete(key);
-          console.log(`Client ${key} disconnected`);
-        }
-      });
+      if (this.clients.has(client.id)) {
+        this.clients.delete(client.id);
+        console.log(`Client ${client.id} disconnected`);
+      }
     }
   }
 }
